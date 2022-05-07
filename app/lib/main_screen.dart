4d632d557e39feb7a749ae4330d60ad4aed19c9a
blueskyson <blueskyson1401@gmail.com>
2022-05-07 17:25:52 +0800

    Create main_screen.dart

---
+++ app/lib/main_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 2;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Search',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Chat',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Home (Put live2D here)',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Stock',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Account',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Financial Management GF'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.fromString('chat-processing')),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.fromString('home')),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.fromString('finance')),
+            label: 'Stock',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.fromString('account')),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
