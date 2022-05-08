5b25897f28e63dbddd9d0fe3376aad81f2e3d631
NotA1ive <55247135+NotA1ive@users.noreply.github.com>
2022-05-08 09:13:16 +0800

    checkpoint

--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-
+import 'package:circular_menu/circular_menu.dart';
 class HomePage extends StatefulWidget{
   const HomePage({Key? key}) : super(key: key);
   @override
@@ -11,18 +11,41 @@ class HomePage extends StatefulWidget{
 
 class _HomePageState extends State<HomePage>{
   static const Widget _image = Image(image: AssetImage('assets/pic/hiyori.png'));
+  /*final _circularMenu = CircularMenu(items: [
+    CircularMenuItem(icon: Icons.home, onTap: () {
+      // callback
+    }),
+    CircularMenuItem(icon: Icons.search, onTap: () {
+      //callback
+    }),
+    CircularMenuItem(icon: Icons.settings, onTap: () {
+      //callback
+    }),
+    CircularMenuItem(icon: Icons.star, onTap: () {
+      //callback
+    }),
+    CircularMenuItem(icon: Icons.pages, onTap: () {
+      //callback
+    }),
+  ]);*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         height: 800.0,
         width: 500.0,
-        padding: EdgeInsets.only(top: 150,bottom:0),
+        padding: const EdgeInsets.only(top: 150,bottom:0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(200),
         ),
-        child: Center(
-          child: _image,
+        child: Stack(
+          children:[
+            GestureDetector(child:Center(child: _image,),onLongPress: ()=>{
+                debugPrint('Long')
+              },
+            ),
+            //_circularMenu,
+          ],
         ),
       ),
     );
