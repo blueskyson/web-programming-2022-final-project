29fc8508f5f409b9e45ad698a9ebae55d65c9b38
NotA1ive <55247135+NotA1ive@users.noreply.github.com>
2022-05-10 20:51:59 +0800

    add golang server for L2D serving

---
+++ v2d/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	p := "." + r.URL.Path
+	if p == "./" {
+		p = "index.html"
+	}
+	http.ServeFile(w, r, p)
+}
+
+func main() {
+	http.HandleFunc("/", serve)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
