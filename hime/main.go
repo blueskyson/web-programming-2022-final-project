5c52d94a6665f277c2d17d88f838aba2dc4c443f
NotA1ive <55247135+NotA1ive@users.noreply.github.com>
2022-05-13 02:52:48 +0800

    test server complete,session pending

--- hime/main.go
+++ hime/main.go
@@ -3,14 +3,52 @@ package main
 import(
 	"fmt"
 	"net/http"
+	"encoding/json"
+	"errors"
 )
 
+var users = map[string]string{}
+
+type User struct{
+	Username string `json:"username"`
+	PwHash string `json:"pwhash"`
+}
 func login(w http.ResponseWriter,r *http.Request){
-	fmt.Println("login not implemented error")
+	var u User
+	err:=json.NewDecoder(r.Body).Decode(&u)
+	if err!=nil{
+		http.Error(w,err.Error(),http.StatusBadRequest)
+		return
+	}
+	pw,exist:=users[u.Username]
+	if(!exist){
+		http.Error(w,errors.New("User Does Not Exist!").Error(),http.StatusBadRequest)
+		return
+	}else if(pw==u.PwHash){
+		http.Error(w,errors.New("Login Success").Error(),http.StatusOK)
+		return
+	}else{
+		http.Error(w,errors.New("Login Failed").Error(),http.StatusOK)
+		return
+	}
 }
 
 func register(w http.ResponseWriter,r *http.Request){
-	fmt.Println("register is not implemented")
+	var u User
+	err:=json.NewDecoder(r.Body).Decode(&u)
+	if err!=nil{
+		http.Error(w,err.Error(),http.StatusBadRequest)
+		return
+	}
+	_,exist:=users[u.Username]
+	if(exist){
+		http.Error(w,errors.New("Username Taken!").Error(),http.StatusOK)
+		return
+	}else{
+		users[u.Username]=u.PwHash
+		http.Error(w,errors.New("Account Creation Success").Error(),http.StatusOK)
+		return
+	}
 }
 
 func main(){
