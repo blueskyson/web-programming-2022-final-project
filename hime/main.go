e484684741f47957e6ba3c223c6a60672d1b69d9
blueskyson <blueskyson1401@gmail.com>
2022-05-13 02:32:57 +0800

    Print request in main.go

--- hime/main.go
+++ hime/main.go
@@ -1,12 +1,35 @@
 package main
 
 import(
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
 func login(w http.ResponseWriter,r *http.Request){
-	fmt.Println("login not implemented error")
+	var bodyBytes []byte
+	var err error
+	if r.Body != nil {
+		bodyBytes, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Printf("Body reading error: %v", err)
+			return
+		}
+		defer r.Body.Close()
+	}
+
+	if len(bodyBytes) > 0 {
+		var prettyJSON bytes.Buffer
+		if err = json.Indent(&prettyJSON, bodyBytes, "", "\t"); err != nil {
+			fmt.Printf("JSON parse error: %v", err)
+			return
+		}
+		fmt.Println(string(prettyJSON.Bytes()))
+	} else {
+		fmt.Printf("Body: No Body Supplied\n")
+	}
 }
 
 func register(w http.ResponseWriter,r *http.Request){
