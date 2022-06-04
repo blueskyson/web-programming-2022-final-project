5e069184fd6e3c52f121771aac36c4a171ce9eb7
NotA1ive <55247135+NotA1ive@users.noreply.github.com>
2022-06-04 16:17:32 +0800

    Hime Rewrite: implement Post History, Post View, Posting functionality

--- hime/main.go
+++ hime/main.go
@@ -30,6 +30,10 @@ type Auth struct {
 	Token    string `json:"token" binding:"required"`
 }
 
+type PostHistory struct {
+	History []string `json:"history" binding:"required"`
+}
+
 func login(c *gin.Context) {
 	db, err := bolt.Open("./AIO.db", 0666, nil)
 	if err != nil {
@@ -223,6 +227,136 @@ func auth(c *gin.Context) {
 	})
 }
 
+func post(c *gin.Context) {
+	var m map[string]interface{}
+	c.Bind(&m)
+	//trust-based
+	usr := m["username"]
+	if usr == nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Invalid User",
+		})
+		return
+	}
+	db, err := bolt.Open("./AIO.db", 0666, nil)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err.Error(),
+		})
+		return
+	}
+	defer db.Close()
+	postID := uuid.New().String()
+	err = db.Update(func(t *bolt.Tx) error {
+		b := t.Bucket([]byte("post"))
+		if b == nil {
+			return fmt.Errorf("bucket is nil")
+		}
+		err = b.Put([]byte(postID), []byte(fmt.Sprint(m)))
+		if err != nil {
+			return fmt.Errorf("Create Post Failed :%v", err)
+		}
+
+		b = t.Bucket([]byte("posthistory"))
+		if b == nil {
+			return fmt.Errorf("bucket is nil")
+		}
+		val := b.Get([]byte(fmt.Sprint(usr)))
+		var ph *PostHistory
+		if val != nil {
+			err = json.Unmarshal(val, &ph)
+			if err != nil {
+				return err
+			}
+			ph.History = append(ph.History, postID)
+		} else {
+			ph = &PostHistory{History: []string{postID}}
+		}
+		enc, err := json.Marshal(ph)
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte(fmt.Sprintf("%v", usr)), []byte(enc))
+		if err != nil {
+			return fmt.Errorf("Create Post Failed :%v", err)
+		}
+		return nil
+	})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "Create Post Success",
+	})
+}
+
+func view(c *gin.Context) {
+	var m map[string]interface{}
+	c.Bind(&m)
+	//trust-based
+	usr := m["username"]
+	if usr == nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Invalid User",
+		})
+		return
+	}
+	db, err := bolt.Open("./AIO.db", 0666, nil)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err.Error(),
+		})
+		return
+	}
+	defer db.Close()
+	err = db.View(func(t *bolt.Tx) error {
+		b := t.Bucket([]byte("posthistory"))
+		if b == nil {
+			return fmt.Errorf("bucket is nil")
+		}
+		val := b.Get([]byte(fmt.Sprint(usr)))
+		if val != nil {
+			c.Data(http.StatusOK, "application/json", val)
+		}
+		return fmt.Errorf("Invalid User")
+	})
+}
+
+func viewpost(c *gin.Context) {
+	var m map[string]interface{}
+	c.Bind(&m)
+	//trust-based
+	pid := m["postid"]
+	if pid == nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Invalid Post ID",
+		})
+		return
+	}
+	db, err := bolt.Open("./AIO.db", 0666, nil)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err.Error(),
+		})
+		return
+	}
+	defer db.Close()
+	err = db.View(func(t *bolt.Tx) error {
+		b := t.Bucket([]byte("post"))
+		if b == nil {
+			return fmt.Errorf("bucket is nil")
+		}
+		val := b.Get([]byte(fmt.Sprint(pid)))
+		if val != nil {
+			c.Data(http.StatusOK, "application/json", val)
+		}
+		return fmt.Errorf("Invalid Post ID")
+	})
+}
+
 func main() {
 	db, err := bolt.Open("./AIO.db", 0666, nil)
 	if err != nil {
@@ -248,6 +382,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = db.Update(func(t *bolt.Tx) error {
+		_, err := t.CreateBucketIfNotExists([]byte("post"))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Update(func(t *bolt.Tx) error {
+		_, err := t.CreateBucketIfNotExists([]byte("posthistory"))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 	db.Close()
 	err = os.MkdirAll("./uploads", os.ModePerm)
 	if err != nil {
@@ -259,6 +413,9 @@ func main() {
 	r.POST("/reg", reg)
 	r.POST("/login", login)
 	r.POST("/up", up)
-	r.POST("/auth", auth)
+	r.POST("/post", post)
+	r.GET("/post", viewpost)
+	r.GET("/auth", auth)
+	r.GET("/view", view)
 	r.Run(":8647")
 }
